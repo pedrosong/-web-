d1c8104359e20513e3e04740b6ffebde3fd0364f
pedrosong <songchenghao96@gmail.com>
2020-11-19 21:14:07 +0800

    新增vue03作业

---
+++ 2020-11-18-vue03/code/homework/src/components/button.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button @click="handleClick">
+            <slot>default btn</slot>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['disabled'],
+    methods: {
+        handleClick(){
+            if(this.disabled) return;
+            this.$emit('click');
+            console.log('resolved')
+        }
+    }
+}
+</script>
