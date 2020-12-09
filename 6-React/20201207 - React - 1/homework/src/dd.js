a48a48292c62af741781989d1904fdc67444f5af
pedrosong <songchenghao96@gmail.com>
2020-12-09 23:20:45 +0800

    新增react02课程源码

--- 6-React/20201207 - React - 1/homework/src/dd.js
+++ 6-React/20201207 - React - 1/homework/src/dd.js
@@ -1,23 +1,25 @@
 import { Component } from "react";
 
-class Dd extends Component{
-    state = { show: false };
-    changeShow = () => {
-        const {show} = this.state;
-        this.setState({
-            show:!show
-        })
-    };
-    render(){
-        const { title, data } = this.props;
-        const { show } = this.state;
-        return (
-        <dl className={show?"friend-group expanded":"friend-group"}>
-            <dt onClick={this.changeShow}> {title} </dt>
-            {data.map((item,index)=> <dd key={index}> {item} </dd>)}
-        </dl>
-        )
-    }
-};
+class Dd extends Component {
+  state = { show: false };
+  changeShow = () => {
+    const { show } = this.state;
+    this.setState({
+      show: !show,
+    });
+  };
+  render() {
+    const { title, data } = this.props;
+    const { show } = this.state;
+    return (
+      <dl className={show ? "friend-group expanded" : "friend-group"}>
+        <dt onClick={this.changeShow}> {title} </dt>
+        {data.map((item, index) => (
+          <dd key={index}> {item} </dd>
+        ))}
+      </dl>
+    );
+  }
+}
 
 export default Dd;
