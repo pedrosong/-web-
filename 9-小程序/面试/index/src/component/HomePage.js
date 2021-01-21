5646f417e7bcbcddb61dad0cfde5eecd1fe605b9
pedrosong <songchenghao96@gmail.com>
2021-01-21 21:49:33 +0800

    新增微信小程序04课程源码及课后作业

--- 9-小程序/面试/index/src/component/HomePage.js
+++
@@ -1,7 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function HomePage(){
-    return (
-        <Link to='/todoList' >todolist</Link>
-    )
-}
--- 9-小程序/面试/index/src/view/Todo.js
+++
@@ -1,8 +0,0 @@
-import TodoList from "./TodoList";
-
-export default function Todo(props){
-    const {id,todo,isDone} = props.data;
-    return <div className={isDone?'done':''}>
-        id:{id}, todo:{todo}, isDone:{isDone?'Done':'Not Done'}
-    </div>
-}
